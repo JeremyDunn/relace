543d175cbb5db620c263faab7a03042e922567d6
Jason McCreary <jason@pureconcepts.net>
2014-08-28 09:11:25 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Relace - A Twitter Bootstrap Migration Tool
+This is the beta of the alpha.
---
+++ tests/test.php
@@ -0,0 +1,10 @@
+<?php
+	$five_int = 5;
+	$five_string = '5';
+
+	function increment($base) {
+		return $base + 1;
+	}
+
+	echo increment($five_int);
+	echo increment($five_string);
